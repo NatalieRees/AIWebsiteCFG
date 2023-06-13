f583a489f114e641408829e1e0771928cb2ab73c
Natalie Rees <47527933+NatalieRees@users.noreply.github.com>
2023-06-13 11:39:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Sign Up Button
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+function closeForm() {
+  document.getElementById("id01").style.display = "none";
+};
+
+function gohome()
+{
+window.location.href="../index.html"
+};
+
+function updateTxt(field1, field2) {
+  var field1 = document.getElementById(field1).value;
+  var field2 = document.getElementById(field2).value;
+  console.log("Name has been entered");
+  alert('Thank you for contacting us ' + field1 + ' ' + field2);
+};
+
+function PressAlert() {
+  console.log("Sign Up Occured");
+  alert("Thanks for siging up!");
+};
